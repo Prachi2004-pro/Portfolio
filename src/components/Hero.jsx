e62b6636b97e17fb555ce5d1b34c9df5272dfae9
Prachi2004-pro <prachibharadwa2004@gmail.com>
2025-04-07 17:59:18 +0530

    Change1 in Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
               </motion.p>
               <motion.a
                 variants={childVariants}
-                href="/Resume.docx"
+                href="/Resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 download
